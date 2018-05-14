882ceded8705b6d04939f261cc4b3d02fb782076
rwutscher <ricwu01@gmail.com>
2018-05-14 14:06:30 +0200

    initialized project

---
+++ src/main/java/com/eventplaner/EventplanerApplication.java
@@ -0,0 +1,12 @@
+package com.eventplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventplanerApplication.class, args);
+	}
+}
