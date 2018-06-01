e2ee9fa2e972d5a1daeee3e44d310c6e28cabaa1
bschlegel <bschlegel@student.tgm.ac.at>
2018-06-01 17:44:49 +0200

    Added non working code to votefortopic

--- src/main/java/com/eventplaner/tasks/pollTasks/VoteForTopic.java
+++ src/main/java/com/eventplaner/tasks/pollTasks/VoteForTopic.java
@@ -7,16 +7,17 @@ import com.eventplaner.tasks.Task;
 public class VoteForTopic implements Task{
 
     private Poll poll;
-
+    private String topic;
     private User user;
 
-    public VoteForTopic(Poll poll, User user) {
+    public VoteForTopic(Poll poll, User user, String topic) {
         this.poll = poll;
         this.user = user;
+        this.topic = topic;
     }
 
     @Override
     public void execute() {
-
+        this.poll.addPollTopic();
     }
 }
