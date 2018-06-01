a86a1b0fd0ffa4336a3afd95c948f89c78130965
rwutscher <ricwu01@gmail.com>
2018-06-01 08:43:38 +0200

    Added and implemented SaveObject Task

---
+++ src/main/java/com/eventplaner/tasks/SaveObject.java
@@ -0,0 +1,49 @@
+package com.eventplaner.tasks;
+
+import com.eventplaner.HibernateUtils;
+import com.eventplaner.model.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SaveObject<T> implements Task {
+
+    public T object;
+
+    public SaveObject(T object){
+        this.object = object;
+    }
+
+    @Override
+    public void execute() {
+
+        // Adding all model classes to hibernate config
+        Configuration config = HibernateUtils.getConfig(new Class[]{
+                Poll.class,
+                PollTopic.class,
+                User.class,
+                UnregisteredUser.class,
+                RegisteredUser.class,
+                Comment.class,
+                CommentSystem.class
+        });
+
+        SessionFactory factory = null;
+        Session session = null;
+
+        try{
+            factory = config.buildSessionFactory();
+            session = factory.openSession();
+
+            session.beginTransaction();
+            session.save(object);
+            session.getTransaction().commit();
+
+            session.close();
+            factory.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
