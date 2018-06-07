72392638c8dc9a50cdc512a8218284e2efb349c1
ntesanovic-tgm <ntesanovic@student.tgm.ac.at>
2018-06-07 20:53:41 +0200

    initialized userRepository in constructor and changed SaveObject to Repo

--- src/main/java/com/eventplaner/tasks/userTasks/CreateUnregisteredUser.java
+++ src/main/java/com/eventplaner/tasks/userTasks/CreateUnregisteredUser.java
@@ -1,6 +1,7 @@
 package com.eventplaner.tasks.userTasks;
 
 import com.eventplaner.model.UnregisteredUser;
+import com.eventplaner.model.repositories.UserRepository;
 import com.eventplaner.tasks.SaveObject;
 import com.eventplaner.tasks.Task;
 
@@ -8,14 +9,16 @@ public class CreateUnregisteredUser implements Task {
 
     private String email;
     private String uid;
+    private UserRepository userRepository;
 
-    public CreateUnregisteredUser(String email) {
+    public CreateUnregisteredUser(String email, UserRepository userRepository) {
         this.email = email;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void execute() {
         UnregisteredUser user = new UnregisteredUser(this.email);
-        new SaveObject<>(user).execute();
+        userRepository.save(user);
     }
 }
