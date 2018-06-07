a3dca9d2a8ced4d4bea3b3257de08946c16ac2a2
rwutscher <ricwu01@gmail.com>
2018-06-07 09:26:05 +0200

    fixed vote for topic restController

--- src/main/java/com/eventplaner/model/PollTopic.java
+++ src/main/java/com/eventplaner/model/PollTopic.java
@@ -1,9 +1,13 @@
 package com.eventplaner.model;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name="poll_topic")
@@ -11,22 +15,27 @@ public class PollTopic {
 
     @Id
     @Column(name="id")
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
     @Column
     private String description;
 
-    @Column
-    private ArrayList<User> availables;
+    @OneToMany
+    @JoinTable(
+            name = "poll_topic_availables",
+            joinColumns = {@JoinColumn(referencedColumnName = "id", name = "poll_topic_id")},
+            inverseJoinColumns = { @JoinColumn(referencedColumnName = "user_id", name = "user_id") })
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<User> availables;
 
     public PollTopic(String description) {
+        this.id = UUID.randomUUID().toString();
         this.description = description;
         this.availables = new ArrayList<>();
     }
 
     public PollTopic() {
+        this.id = UUID.randomUUID().toString();
         this.availables = new ArrayList<>();
     }
 
@@ -34,7 +43,7 @@ public class PollTopic {
         return description;
     }
 
-    public ArrayList<User> getAvailables() {
+    public List<User> getAvailables() {
         return availables;
     }
 
@@ -45,4 +54,21 @@ public class PollTopic {
     public void removeAvailable(User user){
         this.availables.remove(user);
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAvailables(ArrayList<User> availables) {
+        this.availables = availables;
+    }
+
 }
