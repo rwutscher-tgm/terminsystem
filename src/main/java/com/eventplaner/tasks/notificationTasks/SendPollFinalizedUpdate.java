35366d498f2686410611b1da0c5e06e08775ce44
bschlegel <bschlegel@student.tgm.ac.at>
2018-06-07 08:21:39 +0200

    Added mail to SendPollFinalizedUpdate

--- src/main/java/com/eventplaner/tasks/notificationTasks/SendPollFinalizedUpdate.java
+++ src/main/java/com/eventplaner/tasks/notificationTasks/SendPollFinalizedUpdate.java
@@ -13,6 +13,36 @@ public class SendPollFinalizedUpdate implements Task{
 
     @Override
     public void execute() {
+        final String username = "termimysytemsew2018wtsm@gmail.com";
+        final String password = "*T9:7N(!%gV'ruY>";
 
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("termimysytemsew2018wtsm@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("rwutscher@student.tgm.ac.at"));
+            message.setSubject("Signup");
+
+            message.setContent(messageHtml, "text/html");
+            message.setText("Your Terminplaner Account has been created successfully!"
+                    + "\n\n Have fun!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
     }
 }
