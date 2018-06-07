a0d08c4d9799af94c52b8bb6411907fc33a00afa
Florian Medved <florianmedved@gmx.at>
2018-06-07 14:32:08 +0200

    Revert "Merge branch 'master' of github.com:rwutscher-tgm/terminsystem"
    This reverts commit 993e7c70cab78b1288a45534d48b96d96f73b443, reversing
    changes made to f173fb22eb849d688132e1e163c6c3cfe9ee13cf.

--- src/main/java/com/eventplaner/model/PollTopic.java
+++ src/main/java/com/eventplaner/model/PollTopic.java
@@ -1,13 +1,9 @@
 package com.eventplaner.model;
 
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @Entity
 @Table(name="poll_topic")
@@ -15,27 +11,22 @@ public class PollTopic {
 
     @Id
     @Column(name="id")
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
     @Column
     private String description;
 
-    @OneToMany
-    @JoinTable(
-            name = "poll_topic_availables",
-            joinColumns = {@JoinColumn(referencedColumnName = "id", name = "poll_topic_id")},
-            inverseJoinColumns = { @JoinColumn(referencedColumnName = "user_id", name = "user_id") })
-    @LazyCollection(LazyCollectionOption.FALSE)
-    private List<User> availables;
+    @Column
+    private ArrayList<User> availables;
 
     public PollTopic(String description) {
-        this.id = UUID.randomUUID().toString();
         this.description = description;
         this.availables = new ArrayList<>();
     }
 
     public PollTopic() {
-        this.id = UUID.randomUUID().toString();
         this.availables = new ArrayList<>();
     }
 
@@ -43,7 +34,7 @@ public class PollTopic {
         return description;
     }
 
-    public List<User> getAvailables() {
+    public ArrayList<User> getAvailables() {
         return availables;
     }
 
@@ -54,21 +45,4 @@ public class PollTopic {
     public void removeAvailable(User user){
         this.availables.remove(user);
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setAvailables(ArrayList<User> availables) {
-        this.availables = availables;
-    }
-
 }
