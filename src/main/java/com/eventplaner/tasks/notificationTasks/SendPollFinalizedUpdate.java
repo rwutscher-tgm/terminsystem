635bbcb63ec8143c744c219a0d3737995c891321
bschlegel <bschlegel@student.tgm.ac.at>
2018-06-07 09:15:32 +0200

    Added missing imports to notifier tasks

--- src/main/java/com/eventplaner/tasks/notificationTasks/SendPollFinalizedUpdate.java
+++ src/main/java/com/eventplaner/tasks/notificationTasks/SendPollFinalizedUpdate.java
@@ -2,6 +2,10 @@ package com.eventplaner.tasks.notificationTasks;
 
 import com.eventplaner.model.Poll;
 import com.eventplaner.tasks.Task;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 public class SendPollFinalizedUpdate implements Task{
 
--- src/main/java/com/eventplaner/tasks/notificationTasks/SendSignUpSuccess.java
+++ src/main/java/com/eventplaner/tasks/notificationTasks/SendSignUpSuccess.java
@@ -1,10 +1,15 @@
 package com.eventplaner.tasks.notificationTasks;
 
 
+
 import com.eventplaner.model.Comment;
 import com.eventplaner.model.CommentSystem;
 import com.eventplaner.model.RegisteredUser;
 import com.eventplaner.tasks.Task;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 
 
