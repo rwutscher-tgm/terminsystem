9441ea6ac9eb6bca31f33989ad9aadf285241ed0
ntesanovic-tgm <ntesanovic@student.tgm.ac.at>
2018-06-07 21:41:41 +0200

    worked on CommentTasks

--- src/main/java/com/eventplaner/tasks/commentTasks/AddComment.java
+++ src/main/java/com/eventplaner/tasks/commentTasks/AddComment.java
@@ -4,6 +4,8 @@ import com.eventplaner.model.Comment;
 import com.eventplaner.model.CommentSystem;
 import com.eventplaner.model.Poll;
 import com.eventplaner.model.RegisteredUser;
+import com.eventplaner.model.repositories.CommentRepository;
+import com.eventplaner.model.repositories.CommentSystemRepository;
 import com.eventplaner.tasks.SaveObject;
 import com.eventplaner.tasks.Task;
 
@@ -12,24 +14,30 @@ public class AddComment implements Task{
     private CommentSystem system;
     private RegisteredUser author;
     private String comment;
+    private CommentRepository commentRepository;
+    private CommentSystemRepository commentSystemRepository;
 
-    public AddComment(CommentSystem system, RegisteredUser author, String comment){
+    public AddComment(CommentSystem system, RegisteredUser author, String comment, CommentRepository commentRepository, CommentSystemRepository commentSystemRepository){
         this.system = system;
         this.author = author;
         this.comment = comment;
+        this.commentRepository = commentRepository;
+        this.commentSystemRepository = commentSystemRepository;
     }
 
-    public AddComment(Poll poll, RegisteredUser author, String comment){
+    public AddComment(Poll poll, RegisteredUser author, String comment, CommentRepository commentRepository, CommentSystemRepository commentSystemRepository){
         this.system = poll.getCommentSystem();
         this.author = author;
         this.comment = comment;
+        this.commentRepository = commentRepository;
+        this.commentSystemRepository = commentSystemRepository;
     }
 
     @Override
     public void execute() {
         Comment c = new Comment(this.author, this.comment);
         this.system.addComment(c);
-        new SaveObject(c).execute();
-        new SaveObject(this.system).execute();
+        commentRepository.save(c);
+        commentSystemRepository.save(system);
     }
 }
--- src/main/java/com/eventplaner/tasks/commentTasks/RemoveComment.java
+++ src/main/java/com/eventplaner/tasks/commentTasks/RemoveComment.java
@@ -1,18 +1,21 @@
 package com.eventplaner.tasks.commentTasks;
 
 import com.eventplaner.model.Comment;
+import com.eventplaner.model.repositories.CommentRepository;
 import com.eventplaner.tasks.Task;
 
 public class RemoveComment implements Task{
 
     private Comment comment;
+    private CommentRepository commentRepository;
 
-    public RemoveComment(Comment comment) {
+    public RemoveComment(Comment comment, CommentRepository commentRepository) {
         this.comment = comment;
     }
 
     @Override
     public void execute() {
+        commentRepository.delete(comment);
 //        new DeleteObject<>(this.comment);
     }
 }
